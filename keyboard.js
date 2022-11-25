e79437e20e4ad580b0c7b00518151f6c502b9439
bem130 <haruki070617@gmail.com>
2022-11-25 21:26:00 +0900

    make a little

--- keyboard.js
+++ keyboard.js
@@ -1,4 +1,89 @@
-class keyboard {
+class Keyboard {
+    keycodes_to_string(keycodes) {
+        let dic = this.keyname();
+        let rtt = "";
+        for (let keycode of keycodes) {
+            if (dic[Math.abs(keycode)]!=null) {
+                if (keycode>0) {
+                    rtt += dic[keycode];
+                }
+                else {
+                    rtt += dic[-keycode].toUpperCase();
+                }
+            }
+        }
+        return rtt;
+    }
+    s_sort() {
+        let rtl = [];
+        let li = 0;
+        for (let tm of strl) {
+            if (tm.length>li) {
+                li = tm.length;
+            }
+        }
+        this.smax = li;
+        let nic = 0;
+        while (li>=0) {
+            for (let tm of strl) {
+                if (tm.length==li) {
+                    rtl[nic] = tm;
+                    nic++;
+                }
+            }
+            li--;
+        }
+        return rtl;
+    }
+    keyname() {
+        return {
+            44:"a",
+            45:"b",
+            46:"c",
+            47:"d",
+            48:"e",
+            49:"f",
+            50:"g",
+            51:"h",
+            52:"i",
+            53:"j",
+            54:"k",
+            55:"l",
+            56:"m",
+            57:"n",
+            58:"o",
+            59:"p",
+            60:"q",
+            61:"r",
+            62:"s",
+            63:"t",
+            64:"u",
+            65:"v",
+            66:"w",
+            67:"x",
+            68:"y",
+            69:"z",
+        }
+    }
+    passim(keycode) {
+        let rt = [];
+        this.ckeyskeys = Object.keys(this.ckeys)
+        for (let i=0;i<this.ckeys.length;i++) {
+            let ch = ckeys[this.ckeyskeys[i]];
+            for (let ck of ch) {
+                for (let cka of ck) {
+                    if (rt[Math.abs(cka)]==null) {
+                        rt.push(Math.abs(cka));
+                    }
+                }
+            }
+        }
+    }
+    setcparts() {
+        this.ckeys = cparts();
+        this.ckeyskeys = Object.keys(this.ckeys)
+        this.ckeyskeys = this.s_sort(this.ckeyskeys)
+    }
     cparts(file)
     {
         let rtd = {};
